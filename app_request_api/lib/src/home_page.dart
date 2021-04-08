b1ccb95140383150f437a58b8241cd64841507fd
kaleb.silva <kaleb.silva@itbam.org.br>
2021-04-08 11:55:48 -0400

    inicial commit

---
+++ app_request_api/lib/src/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('List Todo\s'),
+      ),
+      body: ListView.builder(
+          itemCount: 20,
+          itemBuilder: (context, index) {
+            return ListTile(title: Text('item $index'));
+          }),
+    );
+  }
+}
